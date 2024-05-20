5a54174b11bb685587db9e244a3577bfa1717fa3
nivancheva <nedyalka.tsintsarova@gmail.com>
2024-05-20 16:31:22 +0300

    add icons for header

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,26 @@
 </script>
 
 <template>
-  <div>Hello world!</div>
+  <header class="header">
+    <div>
+      <img src="/public/headerLogo.svg">
+    </div>
+    <div>
+      <img src="/public/icon-opensea.svg"/>
+      <img src="/public/icon-discord.svg"/>
+      <img src="/public/icon-instagram.svg"/>
+      <img src="/public/icon-twitter.svg"/>
+      <img src="/public/icon-wallet.svg"/>
+    </div>
+  </header>
+  
 </template>
 
 <style scoped>
-
+.header {
+  background-color: black;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
 </style>
