9bedabc1dbb2ec23a9d1ae961039ec6bf9fdbc78
nivancheva <nedyalka.tsintsarova@gmail.com>
2024-05-21 13:48:48 +0300

    responsive nav

--- src/App.vue
+++ src/App.vue
@@ -3,25 +3,26 @@
 </script>
 
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light pt-2 pb-0">
-    <div class="container-fluid d-flex justify-content-between align-items-center">
+  <nav class="navbar p-5 navbar-expand-lg navbar-light pt-2 pb-0">
+    <div class="nav-container container-fluid d-flex justify-content-between align-items-center my-3">
       <div>
         <img src="/headerLogo.svg">
       </div>
-      <div class="d-flex align-items-center icons-container">
-        <a class="btn social-links">
-          <span class="icon icon-discord"></span>
-        </a>
-        <a class="btn social-links">
-          <span class="icon icon-instagram"></span>
-        </a>
-        <a class="btn social-links">
-          <span class="icon icon-twitter"></span>
-        </a>
-        <a class="btn social-links">
-          <span class="icon icon-opensea"></span>
-        </a>
-
+      <div class="links">
+        <div class="align-items-center icons-container">
+          <a class="btn social-links">
+            <span class="icon icon-discord"></span>
+          </a>
+          <a class="btn social-links">
+            <span class="icon icon-instagram"></span>
+          </a>
+          <a class="btn social-links">
+            <span class="icon icon-twitter"></span>
+          </a>
+          <a class="btn social-links">
+            <span class="icon icon-opensea"></span>
+          </a>
+        </div>
         <a class="d-flex align-items-center btn btn-sm btn-connect">
           <span class="icon icon-wallet"></span>
           <span class="text-uppercase ps-2">Staking</span>   
@@ -65,9 +66,33 @@
   box-shadow: rgba(228, 152, 239, 0.25) 0px 0px 20px 5px;
 }
 
+.icons-container {
+  display: flex;
+}
 
 .social-links:hover,
 .btn-connect:hover {
   box-shadow: rgba(228, 152, 239, 0.3) 0px 0px 20px 10px;
+}
+
+@media (max-width: 768px) {
+  .icons-container {
+    display: block;
+    text-align: center;
+    position: fixed;
+    right: 3rem;
+    top: 6rem;
+    z-index: 10;
+  }
+
+  .social-links {
+    margin-bottom: .5rem;
+  }
+}
+
+@media (min-width: 769px) {
+  .links {
+    display: flex;
+  }
 }
 </style>
