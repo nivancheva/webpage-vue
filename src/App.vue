4e0bee60df104d7fcb611e92423e26ba9f622c8c
nivancheva <nedyalka.tsintsarova@gmail.com>
2024-05-22 12:26:00 +0300

    add vertical nav

--- src/App.vue
+++ src/App.vue
@@ -31,17 +31,33 @@
     </div>
   </nav>
 
-  <section class="first-sec container-fluid p-5 row justify-content-center">
-    <img class="cover-photo" src='/cover.png'/>
-    <div class="logo-section">
-      <img src="/logo.png">
-    </div>
-    
-    <div class="col-11 pb-5">
-      <h1 class="text-uppercase mb-0">Molly</h1>
-      <h3>Unuque 3D Metaverse Ready NFTs</h3>
-    </div>
-  </section>
+  <div class="page-content">
+  
+    <nav class="vertical-nav">
+      <ul class="nav-list">
+        <li>Home</li>
+        <li>About</li>
+        <li>Vision</li>
+        <li>Benefits</li>
+        <li>Road</li>
+        <li>Team</li>
+      </ul>
+    </nav>
+
+    <section class="first-sec">
+      <div class="container-fluid row p-5 justify-content-center">
+        <div class="logo-section">
+          <img src="/logo.png">
+        </div>
+        
+        <div class=" title">
+          <h1 class="text-uppercase mb-0">Molly</h1>
+          <h3>Unuque 3D Metaverse Ready NFTs</h3>
+        </div>
+      </div>
+    </section>
+
+  </div>
   
 </template>
 
@@ -87,26 +103,78 @@
   box-shadow: rgba(228, 152, 239, 0.3) 0px 0px 20px 10px;
 }
 
+.page-content {
+  position:relative
+}
+
 .first-sec {
   position: relative;
   margin-inline: auto;
-}
+  min-height: 100vh;
 
-.first-sec .cover-photo {
-  position:absolute;
-  z-index: -1;
-
-  width: 100%;
-  max-width: 1200px;
-  height: 100%;
+  background-image: url('./cover.png');
+  background-position:top center;
+  background-repeat: no-repeat;
+  background-size: contain;
 }
 
 .logo-section {
   display: grid;
   place-content: center;
 
-  max-width: 488px;
+  max-width: 450px;
   width: 100%;
+
+  position: relative;
+}
+
+.title {
+  position: absolute;
+  bottom: 0;
+  left: 5rem
+}
+
+.vertical-nav {
+  position: fixed;
+  z-index: 9;
+  right: 100px;
+  bottom: 30px;
+  width: 65%;
+  max-width: 300px;
+  max-height: 90%;
+  overflow-y: auto;
+  transform: scale(0);
+  transform-origin: right bottom;
+  transition: transform .2s;
+  border-radius: .25em;
+  background-color: rgba(0, 0, 0, .9);
+}
+
+.nav-list {
+    display: table-cell;
+    vertical-align: middle;
+    text-align: center;
+    list-style-type: none;
+    text-transform: uppercase;
+}
+
+.vertical-nav a {
+    display: block;
+    padding: 1em;
+    color: #fff!important;
+    font-weight: 700
+}
+
+.vertical-nav a.active {
+    color: rgba(228,152,239,.8)!important
+}
+
+.ertical-nav.open {
+    transform: scale(1);
+    -webkit-overflow-scrolling: touch;
+    box-shadow: 0 0 20px 10px rgba(228,152,239,.3)!important;
+    -moz-box-shadow: 0 0 20px 10px rgba(228,152,239,.3)!important;
+    -webkit-box-shadow: 0 0 20px 10px rgba(228,152,239,.3)!important
 }
 
 @media (max-width: 768px) {
@@ -122,11 +190,44 @@
   .social-links {
     margin-bottom: .5rem;
   }
+
+  .title {
+    text-align: center;
+    left: 0;
+  }
 }
 
 @media (min-width: 769px) {
   .links {
     display: flex;
   }
+
+  .vertical-nav {
+    right: 0;
+    top: 0;
+    bottom: auto;
+    height: 100vh;
+    width: 90px;
+    max-width: none;
+    max-height: none;
+    transform: scale(1);
+    background-color: transparent;
+    display: table;
+  }
+
+  .vertical-nav:before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, .8);
+    transform: translateX(100%);
+    transition: transform .4s;
+  }
+
+  
+
 }
 </style>
